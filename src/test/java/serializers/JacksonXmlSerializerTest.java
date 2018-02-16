1c1c76b436fb12bd8061e33d276df231831ff709
Asia <joanna.legiedz@gmail.com>
2018-02-16 12:40:22 +0100

    fixing folder structure

--- src/test/java/serializers/JacksonXmlSerializerTest.java
+++ src/test/java/serializers/JacksonXmlSerializerTest.java
@@ -1,16 +1,29 @@
-package serializers;
-
 import javaModel.Joke;
+import serializers.JacksonXmlSerializer;
 import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
-
+@RunWith(JUnit4.class)
 public class JacksonXmlSerializerTest {
+
+    JacksonXmlSerializer jacksonXmlserializer;
+    File file;
+
+    @Before
+    public void setup() {
+        file = new File("jackson_file.xml");
+        jacksonXmlserializer = new JacksonXmlSerializer();
+
+    }
 
     @After
     public void after() {
@@ -19,12 +32,23 @@ public class JacksonXmlSerializerTest {
     }
 
     @Test
-    public void whenJacksonSerializerCreatesAFile() throws IOException {
-        JacksonXmlSerializer serializer = new JacksonXmlSerializer();
-        File file = new File("jackson_file.xml");
-        serializer.serialize(new ArrayList<Joke>());
+    public void doesJacksonSerializerCreateAFile() throws IOException {
+        jacksonXmlserializer.serialize(new ArrayList<Joke>());
         Assert.assertTrue(file.exists());
 
     }
+
+    @Test
+    public void checkXmlXstreamLength() {
+        Joke joke = new Joke(1, "");
+        List<Joke> testList = new ArrayList<>();
+        String xml = "<jokes><joke id=\"1\"></joke></jokes>";
+        int expectedNumberOfBytes = xml.getBytes().length;
+        testList.add(joke);
+        jacksonXmlserializer.serialize(testList);
+        int actualNumberOfBytes = (int) file.length();
+        Assert.assertEquals(expectedNumberOfBytes, actualNumberOfBytes);
+    }
+
 
 }
